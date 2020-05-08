6ff2c2c34b0ca32e65659b00ac4b232d209baad7
Robert Ng <robert.ng@xandr.com>
2020-05-07 22:29:13 -0400

    Database demo

--- class-demos/todoapp/app.py
+++ class-demos/todoapp/app.py
@@ -1,17 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-# creates application that gets named after the name of our file
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rng@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+# create models for database
+db.create_all()
+
+# define route that listens to the below
+@app.route('/todos/create', methods=['POST'])
+# handler
+def create_todo():
+  description = request.form.get('description', '')
+  # create from class
+  todo = Todo(description=description)
+  db.session.add(todo)
+  db.session.commit()
+  # redirect to index page
+  return redirect(url_for('index'))
+
 
 # listens to our homepage
 @app.route('/')
 def index():
     # returns html file
     # pass in variables we want to pass into our template
-    return render_template('index.html', data=[{
-        'description': 'Todo 1'
-    }, {
-        'description': 'Todo 2'
-    }, {
-        'description': 'Todo 3'
-    }])
+    # include data from our database
+    return render_template('index.html', data=Todo.query.all())
