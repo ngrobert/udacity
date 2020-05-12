dea68c6ab8336d7369abe1a9d5b9a042f45eb548
Robert Ng <robert.ng@xandr.com>
2020-05-12 19:37:40 -0400

    Update crud app

--- class-demos/todoapp/app.py
+++ class-demos/todoapp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
@@ -7,16 +8,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rng@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# handle database migrations
+migrate = Migrate(app, db)
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    # add completed column to test migration
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
-
-# create models for database
-db.create_all()
 
 # define route that listens to the below
 @app.route('/todos/create', methods=['POST'])
@@ -42,6 +45,31 @@ def create_todo():
     else:
         return jsonify(body)
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({ 'success': True })
+
 
 # listens to our homepage
 @app.route('/')
@@ -49,4 +77,5 @@ def index():
     # returns html file
     # pass in variables we want to pass into our template
     # include data from our database
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
+
