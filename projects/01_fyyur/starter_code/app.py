59ae67ea4bf33c4cde2733504a56e2d8300584f4
Robert Ng <robert.ng@xandr.com>
2020-05-17 23:42:41 -0400

    Connect to database

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -9,6 +9,7 @@ from flask import Flask, render_template, request, Response, flash, redirect, ur
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from config import Config
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
@@ -18,13 +19,12 @@ from forms import *
 #----------------------------------------------------------------------------#
 
 app = Flask(__name__)
+app.config.from_object(Config)
 moment = Moment(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rng@localhost:5432/fyyur'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# TODO: connect to a local postgresql database
 migrate = Migrate(app, db)
+
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
@@ -34,30 +34,39 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genre = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    website = db.Column(db.String(500))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    seeking_talent = db.Column(db.String(120))
+    seeking_talent_description = db.Column(db.String(500))
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    website = db.Column(db.String(500))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_performance = db.Column(db.String(120))
+    seeking_performance_description = db.Column(db.String(500))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+# class Show(db.Model):
+#   __tablename__ = 'Show'
+#   id = db.Column(db.Integer, primary_key=True)
 
 #----------------------------------------------------------------------------#
 # Filters.
--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -1,13 +1,13 @@
 import os
-SECRET_KEY = os.urandom(32)
-# Grabs the folder where the script runs.
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Enable debug mode.
-DEBUG = True
+class Config:
+    SECRET_KEY = os.urandom(32)
+    # Grabs the folder where the script runs.
+    basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Connect to the database
+    # Enable debug mode.
+    DEBUG = True
 
-
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+    # Connect to the database
+    SQLALCHEMY_DATABASE_URI = 'postgres://rng@localhost:5432/fyyur'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
